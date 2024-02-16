7889d786852524b265e627cc2282f266b6ad6992
MartinKalema <kalemaaaaaaaa@gmail.com>
2024-02-16 22:23:07 +0300

    C1

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+/**
+ * Functional component representing the main app.
+ * @return {JSX.Element} JSX representing the app component.
+ */
+
+export default function App() {
+  return (
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  );
+}
