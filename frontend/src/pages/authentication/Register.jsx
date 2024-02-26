b83ad93151d9ff7642d3e060df701bb0c6f96f82
MartinKalema <kalemaaaaaaaa@gmail.com>
2024-02-26 23:48:04 +0300

    Registration form

--- frontend/src/pages/authentication/Register.jsx
+++ frontend/src/pages/authentication/Register.jsx
@@ -34,6 +34,73 @@ const Register = () => {
     <section className="pl-[10rem] flex flex-wrap">
       <div className="mr-[4rem] mt-[5rem]">
         <h1 className="text-2xl font-semibold mb-4">Register</h1>
+
+        <form className="container w-[40rem]">
+          <div className="my-[2rem]">
+            <label
+              htmlFor="name"
+              className="block text-sm font-medium text-white"
+            >
+              Name
+            </label>
+            <input
+              type="text"
+              id="name"
+              className="mt-1 p-2 border rounded w-full"
+              placeholder="Enter fullname"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </div>
+          <div className="my-[2rem]">
+            <label
+              htmlFor="email"
+              className="block text-sm font-medium text-white"
+            >
+              Email
+            </label>
+            <input
+              type="text"
+              id="email"
+              className="mt-1 p-2 border rounded w-full"
+              placeholder="Enter email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="my-[2rem]">
+            <label
+              htmlFor="password"
+              className="block text-sm font-medium text-white"
+            >
+              Password
+            </label>
+            <input
+              type="text"
+              id="password"
+              className="mt-1 p-2 border rounded w-full"
+              placeholder="Enter password"
+              value={username}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <div className="my-[2rem]">
+            <label
+              htmlFor="confirmPassword"
+              className="block text-sm font-medium text-white"
+            >
+              Confirm Password
+            </label>
+            <input
+              type="text"
+              id="confirmPassword"
+              className="mt-1 p-2 border rounded w-full"
+              placeholder="Confirm Password"
+              value={username}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+            />
+          </div>
+        </form>
       </div>
     </section>
   );
